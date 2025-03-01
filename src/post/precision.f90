0b9fbb34d6f51a4fa93cccdf09a193b2ad12f2d4
toya42 <toya42.fortran@gmail.com>
2025-03-01 14:43:13 +0900

    [delete] post

--- src/post/precision.f90
+++
@@ -1,9 +0,0 @@
-!=====================================================================
-! Module: precision
-! Purpose: Define the floating-point kind (e.g. real64) for use throughout.
-!=====================================================================
-module precision
-  use, intrinsic :: iso_fortran_env, only: real64, int32
-  implicit none
-  integer(int32), parameter :: fp_kind = real64  ! Change to real32 if desired.
-end module precision
